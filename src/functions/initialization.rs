62e770b537823e24523d02106c042f1f12a72b4e
kanarus <kanarus786@gmail.com>
2025-06-22 00:10:30 +0900

    functions/initialization

--- src/functions/initialization.rs
+++ src/functions/initialization.rs
@@ -3,6 +3,8 @@
 //! This section contains functions that load/initialize the model or other data structures. Their use is well illustrated in the code samples.
 
 use crate::{MjLrOpt, MjOption, MjVisual, MjModel, MjData, MjSpec};
+use crate::{ObjectId, obj};
+use crate::MjError;
 
 /// Set default options for length range computation.
 /* void mj_defaultLROpt(mjLROpt* opt); */
@@ -43,8 +45,14 @@ pub fn mj_defaultVisual() -> MjVisual {
 /// Copy `MjModel`.
 /// Unsafely overwrite `dest` if its `Some(&mut _)` with returning `None`,
 /// otherwise allocate new and return it.
+/// 
+/// ## SAFETY
+/// 
+/// When `dest` is `Some(&mut _)`, `src` and it **MUST** have the same sizes structure.
+/// (e.g. same `nq`, `nv`, `nbody`, ...)
+/// 
 /* mjModel* mj_copyModel(mjModel* dest, const mjModel* src); */
-pub fn mj_copyModel(dest: Option<&mut MjModel>, src: &MjModel) -> Option<MjModel> {
+pub unsafe fn mj_copyModel(dest: Option<&mut MjModel>, src: &MjModel) -> Option<MjModel> {
     match dest {
         Some(dest) => {
             unsafe { crate::bindgen::mj_copyModel(dest.as_mut(), src.as_ref()) };
@@ -68,12 +76,308 @@ pub fn mj_saveModel(
     buffer: Option<&mut [u8]>,
 ) {
     let filename = filename.map(|s| std::ffi::CString::new(s.into()).unwrap());
+    let buffer_sz = buffer.as_ref().map_or(0, |b| b.len() as i32);
     unsafe {
         crate::bindgen::mj_saveModel(
             m.as_ref(),
             filename.map_or(std::ptr::null(), |cstr| cstr.as_ptr()),
-            buffer.map_or(std::ptr::null_mut(), |b| b.as_mut_ptr()),
-            buffer.map_or(0, |b| b.len() as i32),
+            buffer.map_or(std::ptr::null_mut(), |b| b.as_mut_ptr() as *mut std::ffi::c_void),
+            buffer_sz,
         );
+    }
+}
+
+/// Load model from binary MJB file.
+/* mjModel* mj_loadModel(const char* filename, const mjVFS* vfs); */
+pub fn mj_loadModel(
+    filename: impl Into<String>,
+) -> MjModel {
+    let filename = std::ffi::CString::new(filename.into()).unwrap();
+    let c_ptr = unsafe {
+        crate::bindgen::mj_loadModel(
+            filename.as_ptr(),
+            std::ptr::null(), // no vfs
+        )
+    };
+    #[cfg(debug_assertions)] {
+        assert!(!c_ptr.is_null(), "Failed to load model from file: {}", filename.to_string_lossy());
+    }
+    MjModel::from(unsafe { *c_ptr })
+}
+
+/// Free memory allocation in model.
+/* void mj_deleteModel(mjModel* m); */
+pub fn mj_deleteModel(m: &mut MjModel) {
+    unsafe { crate::bindgen::mj_deleteModel(m.as_mut()) };
+}
+
+/// Return size of buffer needed to hold model.
+/* int mj_sizeModel(const mjModel* m); */
+pub fn mj_sizeModel(m: &MjModel) -> usize {
+    unsafe { crate::bindgen::mj_sizeModel(m.as_ref()) as usize }
+}
+
+/// Allocate mjData corresponding to given model. If the model buffer is unallocated
+/// the initial configuration will not be set.
+/* mjData* mj_makeData(const mjModel* m); */
+pub fn mj_makeData(m: &MjModel) -> MjData {
+    let c_ptr = unsafe { crate::bindgen::mj_makeData(m.as_ref()) };
+    #[cfg(debug_assertions)] {
+        assert!(!c_ptr.is_null(), "Failed to allocate mjData for model");
+    }
+    MjData::from(unsafe { *c_ptr })
+}
+
+/// Copy mjData. `m` is only required to contain the size fields from MJMODEL_INTS.
+/// 
+/// This overwrites `dest` with `src`, without checking if their corresponding models
+/// have the same sizes structure.
+/// 
+/// ## SAFETY
+///
+/// `dest` and `src` **MUST** have the same sizes structure.
+/// (e.g. same `nq`, `nv`, `nbody`, ...)
+/// 
+/* mjData* mj_copyData(mjData* dest, const mjModel* m, const mjData* src); */
+pub unsafe fn mj_copyData(dest: &mut MjData, m: &MjModel, src: &MjData) {
+    unsafe { crate::bindgen::mj_copyData(dest.as_mut(), m.as_ref(), src.as_ref()) };
+}
+
+/*
+mj_resetData
+void mj_resetData(const mjModel* m, mjData* d);
+Reset data to defaults.
+
+mj_resetDataDebug
+void mj_resetDataDebug(const mjModel* m, mjData* d, unsigned char debug_value);
+Reset data to defaults, fill everything else with debug_value.
+
+mj_resetDataKeyframe
+void mj_resetDataKeyframe(const mjModel* m, mjData* d, int key);
+Reset data. If 0 <= key < nkey, set fields from specified keyframe.
+
+mj_markStack
+void mj_markStack(mjData* d);
+Mark a new frame on the mjData stack.
+
+mj_freeStack
+void mj_freeStack(mjData* d);
+Free the current mjData stack frame. All pointers returned by mj_stackAlloc since the last call to mj_markStack must no longer be used afterwards.
+
+mj_stackAllocByte
+void* mj_stackAllocByte(mjData* d, size_t bytes, size_t alignment);
+Allocate a number of bytes on mjData stack at a specific alignment. Call mju_error on stack overflow.
+
+mj_stackAllocNum
+mjtNum* mj_stackAllocNum(mjData* d, size_t size);
+Allocate array of mjtNums on mjData stack. Call mju_error on stack overflow.
+
+mj_stackAllocInt
+int* mj_stackAllocInt(mjData* d, size_t size);
+Allocate array of ints on mjData stack. Call mju_error on stack overflow.
+
+mj_deleteData
+void mj_deleteData(mjData* d);
+Free memory allocation in mjData.
+
+mj_resetCallbacks
+void mj_resetCallbacks(void);
+Reset all callbacks to NULL pointers (NULL is the default).
+
+mj_setConst
+void mj_setConst(mjModel* m, mjData* d);
+Set constant fields of mjModel, corresponding to qpos0 configuration.
+
+mj_setLengthRange
+int mj_setLengthRange(mjModel* m, mjData* d, int index,
+                      const mjLROpt* opt, char* error, int error_sz);
+Set actuator_lengthrange for specified actuator; return 1 if ok, 0 if error.
+
+mj_makeSpec
+mjSpec* mj_makeSpec(void);
+Create empty spec.
+
+mj_copySpec
+mjSpec* mj_copySpec(const mjSpec* s);
+Copy spec.
+
+mj_deleteSpec
+void mj_deleteSpec(mjSpec* s);
+Free memory allocation in mjSpec.
+
+mjs_activatePlugin
+int mjs_activatePlugin(mjSpec* s, const char* name);
+Activate plugin. Returns 0 on success.
+
+mjs_setDeepCopy
+int mjs_setDeepCopy(mjSpec* s, int deepcopy);
+Turn deep copy on or off attach. Returns 0 on success.
+*/
+
+/// Reset data to defaults.
+/* void mj_resetData(const mjModel* m, mjData* d); */
+pub fn mj_resetData(m: &MjModel, d: &mut MjData) {
+    unsafe { crate::bindgen::mj_resetData(m.as_ref(), d.as_mut()) }
+}
+
+/// Reset data to defaults, fill everything else with debug_value.
+/* void mj_resetDataDebug(const mjModel* m, mjData* d, unsigned char debug_value); */
+pub fn mj_resetDataDebug(m: &MjModel, d: &mut MjData, debug_value: u8) {
+    unsafe { crate::bindgen::mj_resetDataDebug(m.as_ref(), d.as_mut(), debug_value) }
+}
+
+/// Reset data. If `key` is given, set fields from specified keyframe.
+/* void mj_resetDataKeyframe(const mjModel* m, mjData* d, int key); */
+pub fn mj_resetDataKeyframe(
+    m: &MjModel,
+    d: &mut MjData,
+    key: Option<ObjectId<obj::Key>>,
+) {
+    unsafe {
+        crate::bindgen::mj_resetDataKeyframe(
+            m.as_ref(),
+            d.as_mut(),
+            key.map_or(-1, |k| k.index() as i32)
+        )
+    }
+}
+
+/// Mark a new frame on the mjData stack.
+/* void mj_markStack(mjData* d); */
+pub fn mj_markStack(d: &mut MjData) {
+    unsafe { crate::bindgen::mj_markStack(d.as_mut()) }
+}
+
+/// Free the current mjData stack frame. All pointers returned by `mj_stackAlloc` since the last call to `mj_markStack` must no longer be used afterwards.
+/* void mj_freeStack(mjData* d); */
+pub fn mj_freeStack(d: &mut MjData) {
+    unsafe { crate::bindgen::mj_freeStack(d.as_mut()) }
+}
+
+/// Allocate a number of bytes on mjData stack at a specific alignment. Call `mju_error` on stack overflow.
+/* void* mj_stackAllocByte(mjData* d, size_t bytes, size_t alignment); */
+pub fn mj_stackAllocByte(d: &mut MjData, bytes: usize, alignment: usize) -> *mut u8 {
+    unsafe { crate::bindgen::mj_stackAllocByte(d.as_mut(), bytes, alignment) as *mut u8 }
+}
+
+/// Allocate array of `mjtNums` on mjData stack. Call `mju_error` on stack overflow.
+/* mjtNum* mj_stackAllocNum(mjData* d, size_t size); */
+pub fn mj_stackAllocNum(d: &mut MjData, size: usize) -> *mut f64 {
+    unsafe { crate::bindgen::mj_stackAllocNum(d.as_mut(), size) as *mut f64 }
+}
+
+/// Allocate array of `ints` on mjData stack. Call `mju_error` on stack overflow.
+/* int* mj_stackAllocInt(mjData* d, size_t size); */
+pub fn mj_stackAllocInt(d: &mut MjData, size: usize) -> *mut i32 {
+    unsafe { crate::bindgen::mj_stackAllocInt(d.as_mut(), size) as *mut i32 }
+}
+
+/// Free memory allocation in mjData.
+/* void mj_deleteData(mjData* d); */
+pub fn mj_deleteData(d: &mut MjData) {
+    unsafe { crate::bindgen::mj_deleteData(d.as_mut()) };
+}
+
+/// Reset all callbacks to NULL pointers (NULL is the default).
+/* void mj_resetCallbacks(void); */
+pub fn mj_resetCallbacks() {
+    unsafe { crate::bindgen::mj_resetCallbacks() };
+}
+
+/// Set constant fields of mjModel, corresponding to `qpos0` configuration.
+/* void mj_setConst(mjModel* m, mjData* d); */
+pub fn mj_setConst(m: &mut MjModel, d: &mut MjData) {
+    unsafe { crate::bindgen::mj_setConst(m.as_mut(), d.as_mut()) };
+}
+
+/// Set actuator length range for specified actuator.
+/* int mj_setLengthRange(mjModel* m, mjData* d, int index,
+                      const mjLROpt* opt, char* error, int error_sz); */
+pub fn mj_setLengthRange(
+    m: &mut MjModel,
+    d: &mut MjData,
+    index: ObjectId<obj::Actuator>,
+    opt: &MjLrOpt,
+) -> Result<(), MjError> {
+    let mut error = MjError::init();
+    let status = unsafe {
+        let (err_ptr, err_len) = error.as_parts();
+        crate::bindgen::mj_setLengthRange(
+            m.as_mut(),
+            d.as_mut(),
+            index.index() as i32,
+            opt.as_ref(),
+            err_ptr,
+            err_len,
+        )
+    };
+    
+    /*
+    <https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#mj-setlengthrange>
+    > return 1 if ok, 0 if error.
+    */
+    if status == 1 {
+        Ok(())
+    } else {
+        Err(error)
+    }
+}
+
+/// Create empty spec.
+/* mjSpec* mj_makeSpec(void); */
+pub fn mj_makeSpec() -> MjSpec {
+    let c_ptr = unsafe { crate::bindgen::mj_makeSpec() };
+    #[cfg(debug_assertions)] {
+        assert!(!c_ptr.is_null(), "Failed to create empty mjSpec");
+    }
+    MjSpec::from(unsafe { *c_ptr })
+}
+
+/// Copy spec.
+/* mjSpec* mj_copySpec(const mjSpec* s); */
+pub fn mj_copySpec(s: &MjSpec) -> MjSpec {
+    let c_ptr = unsafe { crate::bindgen::mj_copySpec(s.as_ref()) };
+    #[cfg(debug_assertions)] {
+        assert!(!c_ptr.is_null(), "Failed to copy mjSpec");
+    }
+    MjSpec::from(unsafe { *c_ptr })
+}
+
+/// Free memory allocation in mjSpec.
+/* void mj_deleteSpec(mjSpec* s); */
+pub fn mj_deleteSpec(s: &mut MjSpec) {
+    unsafe { crate::bindgen::mj_deleteSpec(s.as_mut()) };
+}
+
+/// Activate plugin.
+/* int mjs_activatePlugin(mjSpec* s, const char* name); */
+pub fn mjs_activatePlugin(s: &mut MjSpec, name: impl Into<String>) -> Result<(), MjError> {
+    let name = std::ffi::CString::new(name.into()).unwrap();
+    let status = unsafe { crate::bindgen::mjs_activatePlugin(s.as_mut(), name.as_ptr()) };
+    
+    /*
+    <https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#mjs-activateplugin>
+    > Returns 0 on success.
+    */
+    if status == 0 {
+        Ok(())
+    } else {
+        Err(MjError::from_error("Failed to activate plugin"))
+    }
+}
+
+/// Turn deep copy on or off attach.
+/* int mjs_setDeepCopy(mjSpec* s, int deepcopy); */
+pub fn mjs_setDeepCopy(s: &mut MjSpec, deepcopy: bool) -> Result<(), MjError> {
+    let status = unsafe { crate::bindgen::mjs_setDeepCopy(s.as_mut(), deepcopy as i32) };
+    
+    /*
+    <https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#mjs-setdeepcopy>
+    > Returns 0 on success.
+    */
+    if status == 0 {
+        Ok(())
+    } else {
+        Err(MjError::from_error("Failed to set deep copy option"))
     }
 }
