33224ed9259a8319e121bc1b8a52780262067a2f
kanarus <kanarus786@gmail.com>
2025-06-21 21:20:20 +0900

    functions/components

--- src/functions.rs
+++ src/functions.rs
@@ -1,11 +1,13 @@
 #![allow(non_snake_case)]
 
 mod attribute_getters;
+mod components;
 mod main_simulation;
 mod parse_and_compile;
 mod support;
 
 pub use attribute_getters::*;
+pub use components::*;
 pub use main_simulation::*;
 pub use parse_and_compile::*;
 pub use support::*;
---
+++ src/functions/components.rs
@@ -0,0 +1,75 @@
+/* https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#components */
+
+//! These are components of the simulation pipeline, called internally from mj_step, mj_forward and mj_inverse. It is unlikely that the user will need to call them.
+
+/// Run position-dependent computations.
+/* void mj_fwdPosition(const mjModel* m, mjData* d); */
+pub fn mj_fwdPosition(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_fwdPosition(m.as_ref(), d.as_mut()) }
+}
+
+/// Run velocity-dependent computations.
+/* void mj_fwdVelocity(const mjModel* m, mjData* d); */
+pub fn mj_fwdVelocity(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_fwdVelocity(m.as_ref(), d.as_mut()) }
+}
+
+/// Compute actuator force qfrc_actuator.
+/* void mj_fwdActuation(const mjModel* m, mjData* d); */
+pub fn mj_fwdActuation(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_fwdActuation(m.as_ref(), d.as_mut()) }
+}
+
+/// Add up all non-constraint forces, compute qacc_smooth.
+/* void mj_fwdAcceleration(const mjModel* m, mjData* d); */
+pub fn mj_fwdAcceleration(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_fwdAcceleration(m.as_ref(), d.as_mut()) }
+}
+
+/// Run selected constraint solver.
+/* void mj_fwdConstraint(const mjModel* m, mjData* d); */
+pub fn mj_fwdConstraint(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_fwdConstraint(m.as_ref(), d.as_mut()) }
+}
+
+/// Euler integrator, semi-implicit in velocity.
+/* void mj_Euler(const mjModel* m, mjData* d); */
+pub fn mj_Euler(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_Euler(m.as_ref(), d.as_mut()) }
+}
+
+/// Runge-Kutta explicit order-N integrator.
+/* void mj_RungeKutta(const mjModel* m, mjData* d, int N); */
+pub fn mj_RungeKutta(m: &crate::MjModel, d: &mut crate::MjData, n: usize) {
+    unsafe { crate::bindgen::mj_RungeKutta(m.as_ref(), d.as_mut(), n as i32) }
+}
+
+/// Integrates the simulation state using an implicit-in-velocity integrator (either “implicit” or “implicitfast”, see Numerical Integration), and advances simulation time. See mjdata.h for fields computed by this function.
+/* void mj_implicit(const mjModel* m, mjData* d); */
+pub fn mj_implicit(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_implicit(m.as_ref(), d.as_mut()) }
+}
+
+/// Run position-dependent computations in inverse dynamics.
+/* void mj_invPosition(const mjModel* m, mjData* d); */
+pub fn mj_invPosition(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_invPosition(m.as_ref(), d.as_mut()) }
+}
+
+/// Run velocity-dependent computations in inverse dynamics.
+/* void mj_invVelocity(const mjModel* m, mjData* d); */
+pub fn mj_invVelocity(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_invVelocity(m.as_ref(), d.as_mut()) }
+}
+
+/// Apply the analytical formula for inverse constraint dynamics.
+/* void mj_invConstraint(const mjModel* m, mjData* d); */
+pub fn mj_invConstraint(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_invConstraint(m.as_ref(), d.as_mut()) }
+}
+
+/// Compare forward and inverse dynamics, save results in fwdinv.
+/* void mj_compareFwdInv(const mjModel* m, mjData* d); */
+pub fn mj_compareFwdInv(m: &crate::MjModel, d: &mut crate::MjData) {
+    unsafe { crate::bindgen::mj_compareFwdInv(m.as_ref(), d.as_mut()) }
+}
