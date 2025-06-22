8eff6f79133548207b671a4318beb2c8d0a8fbd8
kanarus <kanarus786@gmail.com>
2025-06-22 22:19:18 +0900

    add SAFETY comment

--- src/functions.rs
+++ src/functions.rs
@@ -4,6 +4,7 @@ mod helper {
     pub(crate) fn array_flatslice<T, const M: usize, const N: usize>(
         arr: &[[T; N]; M]
     ) -> &[T] {
+        // SAFETY: `[[T; N]; M]` has the same layout as `[T; M * N]`
         unsafe {std::slice::from_raw_parts(arr.as_ptr() as *const T, M * N)}
     }
 
