e5e2de26c4b4e822d351528eb784c83912ab595d
kanarus <kanarus786@gmail.com>
2025-06-29 05:29:41 +0900

    types/rendering

--- src/types/rendering.rs
+++ src/types/rendering.rs
@@ -2,7 +2,11 @@
 //! 
 //! The names of these struct types are prefixed with mjr.
 
-pub use crate::bindgen::{mjrRect, mjrContext};
+pub use crate::bindgen::{
+    mjrRect, mjrContext,
+    mjtTexture, mjtTextureRole, mjtFramebuffer, mjtDepthMap,
+    mjNAUX, mjMAXMATERIAL, mjNTEXROLE, mjMAXTEXTURE,
+};
 
 derive_fields_mapping!(mjrRect {
     scalars {
@@ -13,98 +17,109 @@ derive_fields_mapping!(mjrRect {
     }
 });
 
-/*
-struct mjrContext_ {                // custom OpenGL context
-  // parameters copied from mjVisual
-  float lineWidth;                  // line width for wireframe rendering
-  float shadowClip;                 // clipping radius for directional lights
-  float shadowScale;                // fraction of light cutoff for spot lights
-  float fogStart;                   // fog start = stat.extent * vis.map.fogstart
-  float fogEnd;                     // fog end = stat.extent * vis.map.fogend
-  float fogRGBA[4];                 // fog rgba
-  int shadowSize;                   // size of shadow map texture
-  int offWidth;                     // width of offscreen buffer
-  int offHeight;                    // height of offscreen buffer
-  int offSamples;                   // number of offscreen buffer multisamples
+derive_fields_mapping!(mjrContext {
+    boolean_flags {
+        glInitialized = "is OpenGL initialized";
+        windowAvailable = "is default/window framebuffer available";
+        windowStereo = "is stereo available for default/window framebuffer";
+        windowDoublebuffer = "is default/window framebuffer double buffered";
+    }
+    scalars {
+        windowSamples: usize = "number of samples for default/window framebuffer";
+        lineWidth: f32 = "line width for wireframe rendering";
+        shadowClip: f32 = "clipping radius for directional lights";
+        shadowScale: f32 = "fraction of light cutoff for spot lights";
+        fogStart: f32 = "fog start = stat.extent * vis.map.fogstart";
+        fogEnd: f32 = "fog end = stat.extent * vis.map.fogend";
+        fogRGBA: [f32; 4] = "fog rgba";
+        shadowSize: i32 = "size of shadow map texture";
+        offWidth: i32 = "width of offscreen buffer";
+        offHeight: i32 = "height of offscreen buffer";
+        offSamples: usize = "number of offscreen buffer multisamples";
+        fontScale: i32 = "font scale";
+        auxWidth: [i32; mjNAUX] = "auxiliary buffer width";
+        auxHeight: [i32; mjNAUX] = "auxiliary buffer height";
+        auxSamples: [i32; mjNAUX] = "auxiliary buffer multisamples";
+        offFBO: u32 = "offscreen framebuffer object";
+        offFBO_r: u32 = "offscreen framebuffer for resolving multisamples";
+        offColor: u32 = "offscreen color buffer";
+        offColor_r: u32 = "offscreen color buffer for resolving multisamples";
+        offDepthStencil: u32 = "offscreen depth and stencil buffer";
+        offDepthStencil_r: u32 = "offscreen depth and stencil buffer for multisamples";
+        shadowFBO: u32 = "shadow map framebuffer object";
+        shadowTex: u32 = "shadow map texture";
+        auxFBO: [u32; mjNAUX] = "auxiliary framebuffer object";
+        auxFBO_r: [u32; mjNAUX] = "auxiliary framebuffer object for resolving";
+        auxColor: [u32; mjNAUX] = "auxiliary color buffer";
+        auxColor_r: [u32; mjNAUX] = "auxiliary color buffer for resolving";
+        mat_texid: [i32; mjMAXMATERIAL * mjNTEXROLE] = "material texture ids (-1: no texture)";
+        mat_texuniform: [i32; mjMAXMATERIAL] = "uniform cube mapping";
+        mat_texrepeat: [f32; mjMAXMATERIAL * 2] = "texture repetition for 2d mapping";
+        ntexture: usize = "number of allocated textures";
+        texture: [u32; mjMAXTEXTURE] = "texture names";
+        basePlane: u32 = "all planes from model";
+        baseMesh: u32 = "all meshes from model";
+        baseHField: u32 = "all height fields from model";
+        baseBuiltin: u32 = "all builtin geoms, with quality from model";
+        baseFontNormal: u32 = "normal font";
+        baseFontShadow: u32 = "shadow font";
+        baseFontBig: u32 = "big font";
+        rangePlane: i32 = "all planes from model";
+        rangeMesh: i32 = "all meshes from model";
+        rangeHField: i32 = "all hfields from model";
+        rangeBuiltin: i32 = "all builtin geoms, with quality from model";
+        rangeFont: i32 = "all characters in font";
+        charWidth: [i32; 127] = "character widths: normal and shadow [px]";
+        charWidthBig: [i32; 127] = "character widths: big [px]";
+        charHeight: i32 = "character heights: normal and shadow [px]";
+        charHeightBig: i32 = "character heights: big [px]";
+        nskin: usize = "number of skins";
+    }
+});
+#[allow(non_snake_case)]
+impl mjrContext {
+    /// default color pixel format for `mjr_readPixels`
+    pub fn readPixelFormat(&self) -> i32 {
+        self.readPixelFormat
+    }
+    /// set default color pixel format for `mjr_readPixels`
+    pub fn set_readPixelFormat(&mut self, value: i32) -> &mut Self {
+        self.readPixelFormat = value; self
+    }
 
-  // parameters specified at creation
-  int fontScale;                    // font scale
-  int auxWidth[mjNAUX];             // auxiliary buffer width
-  int auxHeight[mjNAUX];            // auxiliary buffer height
-  int auxSamples[mjNAUX];           // auxiliary buffer multisamples
+    /// depth mapping: `mjDEPTH_ZERONEAR` or `mjDEPTH_ZEROFAR`
+    pub fn readDepthMap(&self) -> mjtDepthMap {
+        mjtDepthMap(self.readDepthMap as u32)
+    }
+    /// set depth mapping: `mjDEPTH_ZERONEAR` or `mjDEPTH_ZEROFAR`
+    pub fn set_readDepthMap(&mut self, value: mjtDepthMap ) -> &mut Self {
+        self.readDepthMap = value.0 as i32; self
+    }
 
-  // offscreen rendering objects
-  unsigned int offFBO;              // offscreen framebuffer object
-  unsigned int offFBO_r;            // offscreen framebuffer for resolving multisamples
-  unsigned int offColor;            // offscreen color buffer
-  unsigned int offColor_r;          // offscreen color buffer for resolving multisamples
-  unsigned int offDepthStencil;     // offscreen depth and stencil buffer
-  unsigned int offDepthStencil_r;   // offscreen depth and stencil buffer for multisamples
+    /// type of texture (ntexture)
+    pub fn textureType(&self) -> [mjtTexture; mjMAXTEXTURE] {
+        self.textureType.map(|t| mjtTexture(t as u32))
+    }
 
-  // shadow rendering objects
-  unsigned int shadowFBO;           // shadow map framebuffer object
-  unsigned int shadowTex;           // shadow map texture
+    /// currently active framebuffer: `mjFB_WINDOW` or `mjFB_OFFSCREEN`
+    pub fn currentBuffer(&self) -> mjtFramebuffer {
+        mjtFramebuffer(self.currentBuffer as u32)
+    }
 
-  // auxiliary buffers
-  unsigned int auxFBO[mjNAUX];      // auxiliary framebuffer object
-  unsigned int auxFBO_r[mjNAUX];    // auxiliary framebuffer object for resolving
-  unsigned int auxColor[mjNAUX];    // auxiliary color buffer
-  unsigned int auxColor_r[mjNAUX];  // auxiliary color buffer for resolving
-
-  // materials with textures
-  int mat_texid[mjMAXMATERIAL*mjNTEXROLE]; // material texture ids (-1: no texture)
-  int mat_texuniform[mjMAXMATERIAL];       // uniform cube mapping
-  float mat_texrepeat[mjMAXMATERIAL*2];    // texture repetition for 2d mapping
-
-  // texture objects and info
-  int ntexture;                            // number of allocated textures
-  int textureType[mjMAXTEXTURE];           // type of texture (mjtTexture) (ntexture)
-  unsigned int texture[mjMAXTEXTURE];      // texture names
-
-  // displaylist starting positions
-  unsigned int basePlane;           // all planes from model
-  unsigned int baseMesh;            // all meshes from model
-  unsigned int baseHField;          // all height fields from model
-  unsigned int baseBuiltin;         // all builtin geoms, with quality from model
-  unsigned int baseFontNormal;      // normal font
-  unsigned int baseFontShadow;      // shadow font
-  unsigned int baseFontBig;         // big font
-
-  // displaylist ranges
-  int rangePlane;                   // all planes from model
-  int rangeMesh;                    // all meshes from model
-  int rangeHField;                  // all hfields from model
-  int rangeBuiltin;                 // all builtin geoms, with quality from model
-  int rangeFont;                    // all characters in font
-
-  // skin VBOs
-  int nskin;                        // number of skins
-  unsigned int* skinvertVBO;        // skin vertex position VBOs (nskin)
-  unsigned int* skinnormalVBO;      // skin vertex normal VBOs (nskin)
-  unsigned int* skintexcoordVBO;    // skin vertex texture coordinate VBOs (nskin)
-  unsigned int* skinfaceVBO;        // skin face index VBOs (nskin)
-
-  // character info
-  int charWidth[127];               // character widths: normal and shadow
-  int charWidthBig[127];            // chacarter widths: big
-  int charHeight;                   // character heights: normal and shadow
-  int charHeightBig;                // character heights: big
-
-  // capabilities
-  int glInitialized;                // is OpenGL initialized
-  int windowAvailable;              // is default/window framebuffer available
-  int windowSamples;                // number of samples for default/window framebuffer
-  int windowStereo;                 // is stereo available for default/window framebuffer
-  int windowDoublebuffer;           // is default/window framebuffer double buffered
-
-  // framebuffer
-  int currentBuffer;                // currently active framebuffer: mjFB_WINDOW or mjFB_OFFSCREEN
-
-  // pixel output format
-  int readPixelFormat;              // default color pixel format for mjr_readPixels
-
-  // depth output format
-  int readDepthMap;                 // depth mapping: mjDEPTH_ZERONEAR or mjDEPTH_ZEROFAR
-};
-typedef struct mjrContext_ mjrContext;
-*/
+    /// skin vertex position VBOs (nskin)
+    pub fn skinvertVBO(&self) -> &[u32] {
+        unsafe { std::slice::from_raw_parts(self.skinvertVBO, self.nskin()) }
+    }
+    /// skin vertex normal VBOs (nskin)
+    pub fn skinnormalVBO(&self) -> &[u32] {
+        unsafe { std::slice::from_raw_parts(self.skinnormalVBO, self.nskin()) }
+    }
+    /// skin vertex texture coordinate VBOs (nskin)
+    pub fn skintexcoordVBO(&self) -> &[u32] {
+        unsafe { std::slice::from_raw_parts(self.skintexcoordVBO, self.nskin()) }
+    }
+    /// skin face index VBOs (nskin)
+    pub fn skinfaceVBO(&self) -> &[u32] {
+        unsafe { std::slice::from_raw_parts(self.skinfaceVBO, self.nskin()) }
+    }
+}
