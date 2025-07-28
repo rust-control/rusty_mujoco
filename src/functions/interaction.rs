15147cb7fc54f0a109f815e0feefc41778e7fb13
kanarus <kanarus786@gmail.com>
2025-07-28 13:35:36 +0900

    chore: remove forgot-to-delete link in doc comment

--- src/functions/interaction.rs
+++ src/functions/interaction.rs
@@ -1,7 +1,5 @@
 //! # [Interaction](https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#interaction)
 //! 
-//! <https://mujoco.readthedocs.io/en/stable/APIreference/APIfunctions.html#interaction>
-//! 
 //! These functions implement abstract mouse interactions, allowing control over cameras and perturbations.
 //! Their use is well illustrated in [simulate](https://mujoco.readthedocs.io/en/stable/programming/samples.html#sasimulate).
 
